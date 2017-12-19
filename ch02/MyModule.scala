72aedee3f558974cadebb26fae46954673c400f0
Kiellini <ezequiel.aguilarg@gmail.com>
2017-12-19 20:40:47 +0000

    def fibonacci

--- ch02/MyModule.scala
+++ ch02/MyModule.scala
@@ -2,19 +2,33 @@
 
 object MyModule {
     
+    def factorial(n: Int): Int = {
+        @annotation.tailrec
+        def go(n: Int, acc: Int): Int =
+            if(n <= 0)acc
+            else go(n-1, n*acc)
+        go(n, 1)
+    }
+    
+    def formatResult(name: String, n: Int, f: Int=> Int) = {
+        val msg = "The %s of %d is %d."
+        msg.format(name, n, f(n))
+    }
+    
     def abs(n: Int): Int =
         if(n < 0) -n
         else n
         
     
     private def formatAbs(x: Int) = {
-        val msg = "The absolute value of %d is %d"
+        val msg = "The absolute value f %d is %d"
         msg.format(x, abs(x))
         
     }
     
     def main(args: Array[String]): Unit =
         println(formatAbs(-42))
+        println(formatResult("factorial", 7, factorial))
     
     
 }
