610f4ce77b558ee043fbbd059d403053ff1118d0
Kiellini <ezequiel.aguilarg@gmail.com>
2017-12-19 21:19:33 +0000

    polymorphic function

--- ch02/MyModule.scala
+++ ch02/MyModule.scala
@@ -5,6 +5,7 @@ object MyModule {
     def factorial(n: Int): Int = {
         @annotation.tailrec
         def go(n: Int, acc: Int): Int =
+            
             if(n <= 0)acc
             else go(n-1, n*acc)
         go(n, 1)
@@ -26,9 +27,22 @@ object MyModule {
         
     }
     
+    def findFirst[A](as: Array[A], p: A => Boolean): Int ={
+        
+        @annotation.tailrec
+        def loop(n: Int): Int =
+            if(n >= as.length) -1
+            else if(p(as(n))) n
+            else loop(n+1)
+        loop(0)
+        
+        
+    }
+    
     def main(args: Array[String]): Unit =
         println(formatAbs(-42))
         println(formatResult("factorial", 7, factorial))
+        println(findFirst(Array(7, 9, 13), (x: Int) => x ==7))
     
     
 }
